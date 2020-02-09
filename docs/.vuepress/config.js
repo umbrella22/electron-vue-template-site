be0297651f011fcaf8cfb1a53dc9bb4f824f8a44
umbrella22 <syl1998@hotmail.com>
2020-02-09 21:14:41 +0800

    修正错误

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,5 +1,5 @@
 module.exports = {
-    base:'/electrn-vue-template-doc/',
+    base:'/electron-vue-template-doc/',
     title: "electron-vue-template-doc",
     description: "基于vue来构建一个electron应用程序",
     markdown: {
