79697d737ee29a7dd0ea032a7cda1bb7384be614
张恒 <syl1998@hotmail.com>
2020-05-06 14:37:14 +0800

    修改一次

--- deploy.sh
+++ deploy.sh
@@ -16,6 +16,6 @@ git init
 git add -A
 git commit -m 'deploy'
 
-git push -f "https://github.com/umbrella22/electron-vue-template-doc.git" master:gh-pages
+git push -f "https://${access_token}@github.com/umbrella22/electron-vue-template-doc.git" master:gh-pages
 
 cd -
